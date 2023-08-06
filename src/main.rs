09d8c7746a84c26a62f47de9e365ed16aa4b5bf8
Emre Guneyler <eguneys@gmail.com>
2023-08-06 20:05:09 +0300

    push

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::async_trait;
+
+use serenity::framework::StandardFramework;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::CommandResult;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+#[command]
+async fn help(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "A simple test bot").await?;
+    Ok(())
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "pong!").await?;
+    Ok(())
+}
+
+#[group]
+#[commands(help, ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let framework = StandardFramework::new()
+    .group(&GENERAL_GROUP);
+
+    let mut client = Client::builder(&token, GatewayIntents::default())
+    .event_handler(Handler)
+    .framework(framework)
+    .await
+    .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+
 }
